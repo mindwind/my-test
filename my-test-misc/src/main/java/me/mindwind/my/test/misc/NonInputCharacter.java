86c1e8fe3dd345b94d303c03eabcabb89c57304d
mindwind <mindwind@163.com>
2013-08-19 10:38:17 +0800

    new - my-test-misc module

---
+++ my-test-misc/src/main/java/me/mindwind/my/test/misc/NonInputCharacter.java
@@ -0,0 +1,15 @@
+package me.mindwind.my.test.misc;
+
+import java.util.Arrays;
+
+/**
+ * @author mindwind
+ * @version 1.0, Aug 16, 2013
+ */
+public class NonInputCharacter {
+	
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString("\f".getBytes()));
+	}
+	
+}
